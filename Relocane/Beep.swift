4d8f25392f83a3060465dd5527933ce918b10269
Relocane LLC <relocane@gmail.com>
2025-03-19 14:49:33 -0400

    Update Beep.swift

--- Relocane/Beep.swift
+++ Relocane/Beep.swift
@@ -8,10 +8,17 @@ import Foundation
 import AVFoundation
 import SwiftUI
 
+var speaker = Speaker()
+
 class Beep: NSObject, ObservableObject{
     var strength = -100
     var last_strength = -100
     var audio = AVAudioPlayer()
+    var BLE: BLEmanager!
+    var enabled = false
+    var count = 0 //counts how many times the same strength has repeated
+    //when it repeats more than 5? times we say we cant see the device
+    
     override init() {
         super.init()
         guard let file = NSDataAsset(name: "sonar") else {
@@ -27,6 +34,36 @@ class Beep: NSObject, ObservableObject{
     }
     
     func start() {
-        audio.play()
+        enabled = true
+        last_strength = Int(BLE.strength)
+        while (enabled) { //just tried this
+            strength = Int(BLE.strength)
+            if (strength == last_strength) {
+                count += 1
+            }
+            else {
+                count = 0
+            }
+            
+            if (abs(strength-last_strength) > 50) {
+                strength = last_strength
+            }
+            
+            last_strength = strength
+            
+            if (count >= 5 && !BLE.seen(BLE.connectedUUID!)) {
+                speaker.speak(phrase: "Device not found nearby. Stopping scanning.")
+                return
+            }
+            else {
+                audio.play()
+            }
+            sleep(UInt32(10 * Int(round(Double(strength) / 10.0)) * 10))
+            //big ass thing to round the strength
+        }
+    }
+    
+    func stop() {
+        enabled = false
     }
 }
